55176ead7048a8355994a053c3ec7ac1609f7a25
Mateja Bezjak <mateja.mia.bezjak@gmail.com>
2020-01-06 00:28:30 +0100

    Adding to HP

--- src/js/file1.js
+++ src/js/file1.js
@@ -1,1 +1,20 @@
 console.log('Yeeeeesh!');
+// $(document).ready(function(){
+//     $('.b-slider .b-slider__imgwrapper').slick({
+//         autoplay: false,
+//         centerMode: true,
+//         slidesToShow: 1,
+//         dots: true
+//     });
+// });
+// $('.b-slider .b-slider__content').slick({
+//     // infinite: true,
+//     centerMode: true,
+//     slidesToShow: 1,
+//     dots: true,
+//     autoplay: false,
+//     nextArrow: $('b-slider__arrow-right'),
+//     prevArrow: $('b-slider__arrow-left')
+//     //adaptiveHeight: true
+
+// });
