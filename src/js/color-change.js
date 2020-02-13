c4492bdc001f7952f9bff76aa152e9fbb36c9fda
Mateja Bezjak <mateja.mia.bezjak@gmail.com>
2020-02-13 15:29:00 +0100

    Comment

--- src/js/color-change.js
+++ src/js/color-change.js
@@ -1,14 +1,16 @@
 console.log('gimme color!');
+//work in progress
 
-var body = document.getElementById('body');
 
-$(function() {
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 2000) {
-          $(body).addClass('colorChange');
-       }
-       if ($(this).scrollTop() < 2000) {
-          $(body).removeClass('colorChange');
-       }
-    });
- });
+// var body = document.getElementById('body');
+
+// $(function() {
+//     $(window).scroll(function () {
+//        if ($(this).scrollTop() > 2000) {
+//           $(body).addClass('colorChange');
+//        }
+//        if ($(this).scrollTop() < 2000) {
+//           $(body).removeClass('colorChange');
+//        }
+//     });
+//  });
