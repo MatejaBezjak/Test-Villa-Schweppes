7edc19d217f99533619caacfdd345e2f7c490679
Mateja Bezjak <mateja.mia.bezjak@gmail.com>
2020-02-14 16:49:50 +0100

    Small fixes

--- src/js/color-change.js
+++ src/js/color-change.js
@@ -14,3 +14,62 @@ console.log('gimme color!');
 //        }
 //     });
 //  });
+
+// var firstPosition = document.getElementById('firstPoint');
+// var secondPosition = document.getElementById('secondPoint');
+// var firstTopPos = $(firstPosition).height();
+// var secondTopPos = $(secondPosition).height();
+
+
+//    $(window).scroll(function () {
+//          if ($(window).height() > firstTopPos) {
+//             $('body').addClass('colorChange');
+//             console.log('add black');
+   
+//          }
+//          if ($(window).height() > secondTopPos) {
+//             $('body').removeClass('colorChange');
+//             console.log('remove black');
+//          }else{
+//            if ($(window).height() < secondTopPos) {
+//               $('body').addClass('colorChange');
+//               console.log('readd black');
+   
+//            }
+//            if ($(window).height() < firstTopPos) {
+//               $('body').removeClass('colorChange');
+//               console.log('readd white');
+//            }
+//         }
+//    });
+
+
+
+
+// var firstPosition = document.getElementById('firstPoint');
+// var secondPosition = document.getElementById('secondPoint');
+// var firstTopPos = $(firstPosition).offset().top + Math.round(($(firstPosition).height()/100) * 0.7);
+// var secondTopPos = $(secondPosition).offset().top + Math.round(($(secondPosition).height()/100) * 0.7);
+
+
+//    $(window).scroll(function () {
+//          if ($(window).offset().top > firstTopPos) {
+//             $('body').addClass('colorChange');
+//             console.log('add black');
+   
+//          }
+//          if ($(window).offset().top > secondTopPos) {
+//             $('body').removeClass('colorChange');
+//             console.log('remove black');
+//          }else{
+//            if ($(window).offset().top < secondTopPos) {
+//               $('body').addClass('colorChange');
+//               console.log('readd black');
+   
+//            }
+//            if ($(window).offset().top < firstTopPos) {
+//               $('body').removeClass('colorChange');
+//               console.log('readd white');
+//            }
+//         }
+//    });
