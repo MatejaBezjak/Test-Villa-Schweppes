1937b8b1cf2ce1cc44a5dfef9630cd9e4cf125fe
Mateja Bezjak <mateja.mia.bezjak@gmail.com>
2020-03-05 16:56:54 +0100

    Dropdown in progress

--- src/js/submenu.js
+++ src/js/submenu.js
@@ -1,17 +1,55 @@
 console.log('ajmo submenu');
 
+var submenu = $('.b-header__submenu');
+var submenuHeight = submenu.height();
+var currentMousePos = 0;
+
+$(document).mousemove(function(event) {
+    currentMousePos = event.pageY;
+});
+
+
+// $(document).ready(function(){
+//     $('.b-header__navigation-link').hover(
+//         function(){
+//         $(submenu).addClass('active');
+//         console.log('down');
+//         },
+//         function(){
+//             // if(currentMousePos > submenuHeight){
+//                 $(submenu).removeClass('active');
+//                 console.log(currentMousePos);
+//                 console.log(submenuHeight);
+
+//             // }
+            
+
+//         }
+//     );
+// });
+
+
 $(document).ready(function(){
-    $('.b-header__navigation-link').hover(
+    $('.b-header__navigation-link-submenu').hover(
         function(){
-        $('.b-header__submenu').slideDown('medium') && $('.b-header__submenu').css('color', 'yellow');
-        e.stopPropagation()
+        $(submenu).addClass('active');
         console.log('down');
-        },
+        }
+    );
+    // $('.b-header__navigation-link-submenu').mouseout(
+    //     function(){
+    //     $(submenu).removeClass('active');
+    //     console.log('down');
+    //     }
+    // );
+    $(submenu).mouseleave(
         function(){
-        $('.b-header__submenu').slideUp('medium');
-        e.stopPropagation()
-        console.log('up');
+            $(submenu).removeClass('active');
+            console.log(currentMousePos);
+            console.log(submenuHeight);
 
         }
     );
 });
+
+
